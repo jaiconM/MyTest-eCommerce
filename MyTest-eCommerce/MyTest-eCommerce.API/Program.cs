f06f07761a09fb15009e83865f4030ede559b7fc
WAY2\jaicon.marmitt <jaicon.marmitt@way2.com.br>
2022-01-07 13:47:45 -0300

    swagger

--- MyTest-eCommerce/MyTest-eCommerce.API/Program.cs
+++ MyTest-eCommerce/MyTest-eCommerce.API/Program.cs
@@ -23,6 +23,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c => {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyTest-eCommerce v1");
+    c.RoutePrefix = string.Empty;
 });
 
 app.UseHttpsRedirection();
